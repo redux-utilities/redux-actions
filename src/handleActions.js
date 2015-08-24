626ad1675008898300ad885f1e51b89cf8ad15b7
Derek Brans <dbrans@gmail.com>
2015-08-24 13:01:04 -0700

    Can use map instead of reduce here.

--- src/handleActions.js
+++ src/handleActions.js
@@ -2,10 +2,9 @@ import handleAction from './handleAction';
 import reduceReducers from 'reduce-reducers';
 
 export default function handleActions(handlers, defaultState) {
-  const reducers = Object.keys(handlers).reduce((result, type) => {
-    result.push(handleAction(type, handlers[type]));
-    return result;
-  }, []);
+  const reducers = Object.keys(handlers).map(type => {
+    return handleAction(type, handlers[type]);
+  });
 
   return typeof defaultState !== 'undefined'
     ? (state = defaultState, action) => reduceReducers(...reducers)(state, action)
