48721b0dccb0deeb8f2341931ab9518907ee5c7e
Christian Lentfort <unkieee@gmail.com>
2015-10-01 20:48:31 +0200

    Update eslint and config, add flowtypes
    Introduce Flow typing and update eslint and related packages to more recent
    versions. The updates are necessary to prevent flow types to be reported as
    errors.
    
    I also fixed any lint issues with the new config (adding trailing `,` and making
    sure identifiers are longer than 1 char).

--- src/handleAction.js
+++ src/handleAction.js
@@ -1,26 +1,44 @@
+/* @flow */
+
 import { isError } from 'flux-standard-action';
 
-function isFunction(val) {
-  return typeof val === 'function';
-}
+import type {Action} from './createAction';
+import identity from './identity';
 
-export default function handleAction(type, reducers) {
+export type State = Object;
+export type Reducer = (state: State, action: Action) => State;
+export type ReducerMap = {
+  next?: Reducer;
+  throw?: Reducer;
+};
+
+export default function handleAction(
+  type: string,
+  reducers: Reducer | ReducerMap
+): Reducer {
   return (state, action) => {
     // If action type does not match, return previous state
-    if (action.type !== type) return state;
-
-    const handlerKey = isError(action) ? 'throw' : 'next';
-
-    // If function is passed instead of map, use as reducer
-    if (isFunction(reducers)) {
-      reducers.next = reducers;
+    if (action.type !== type) {
+      return state;
     }
 
-    // Otherwise, assume an action map was passed
-    const reducer = reducers[handlerKey];
+    let next;
+    let error;
 
-    return isFunction(reducer)
-      ? reducer(state, action)
-      : state;
+    // If function is passed instead of map, use as reducer
+    if (typeof reducers === 'function') {
+      next = reducers;
+      error = identity;
+    } else {
+      next = reducers.next || identity;
+      error = reducers.throw || identity;
+    }
+
+    const finalReducer = isError(action) ? error : next;
+    if (typeof finalReducer === 'function') {
+      return finalReducer(state, action);
+    }
+
+    return identity(state);
   };
 }
