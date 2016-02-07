87623691fad79987421517a87898011300740d4e
contra <contra@maricopa.edu>
2016-02-07 16:24:18 -0600

    only create reducer function one time
    creating a new function w/ a spread each time is a little wasteful

--- src/handleActions.js
+++ src/handleActions.js
@@ -6,8 +6,9 @@ export default function handleActions(handlers, defaultState) {
   const reducers = ownKeys(handlers).map(type => {
     return handleAction(type, handlers[type]);
   });
+  const reducer = reduceReducers(...reducers)
 
   return typeof defaultState !== 'undefined'
-    ? (state = defaultState, action) => reduceReducers(...reducers)(state, action)
-    : reduceReducers(...reducers);
+    ? (state = defaultState, action) => reducer(state, action)
+    : reducer;
 }
