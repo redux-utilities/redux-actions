4004e0d28450442936e1ecfb6738ad8de420acef
Tim Cheung <tim@cheung.io>
2016-04-23 00:21:23 +0200

    Upgrade ESLint dependencies and fix tests

--- src/handleActions.js
+++ src/handleActions.js
@@ -3,10 +3,8 @@ import ownKeys from './ownKeys';
 import reduceReducers from 'reduce-reducers';
 
 export default function handleActions(handlers, defaultState) {
-  const reducers = ownKeys(handlers).map(type => {
-    return handleAction(type, handlers[type]);
-  });
-  const reducer = reduceReducers(...reducers)
+  const reducers = ownKeys(handlers).map(type => handleAction(type, handlers[type]));
+  const reducer = reduceReducers(...reducers);
 
   return typeof defaultState !== 'undefined'
     ? (state = defaultState, action) => reducer(state, action)
