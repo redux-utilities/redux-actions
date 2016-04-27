d0fe222085749abd467c3b577fb210c1302b6c2d
Alan R. Soares <alanrsoars@gmail.com>
2016-04-27 13:48:14 +1200

    fix function name spacing

--- src/createActions.js
+++ src/createActions.js
@@ -3,7 +3,7 @@ import createAction from './createAction';
 import camelize from './camelize';
 
 // creates a map of camelized actionTypes to the corresponding actions
-export default function createActions (actionTypes, transform = camelize) {
+export default function createActions(actionTypes, transform = camelize) {
   return ownKeys(actionTypes)
     .reduce((acc, type) => ({
       ...acc, [transform(type)]: createAction(type)
