006ffd33ffde27a3272ae86d0b3ec8014d13922c
Alan R. Soares <alanrsoars@gmail.com>
2016-04-27 13:44:55 +1200

    Update createActions.js

--- src/createActions.js
+++ src/createActions.js
@@ -1,13 +1,14 @@
-import createAction from './createAction'
+import ownKeys from './ownKeys';
+import createAction from './createAction';
 
 // MY_CONSTANT_NAME => myConstantName
 const camelize = s => s.toLowerCase()
-                       .replace(/_\w/g, x => x[1].toUpperCase())
+                       .replace(/_\w/g, x => x[1].toUpperCase());
 
 // creates a map of camelized actionTypes to the corresponding actions
-export default function createActions (actionTypes) {
+export default function createActions (actionTypes, transform = camelize) {
   return Object.keys(actionTypes)
     .reduce((acc, type) => ({
-      ...acc, [camelize(type)]: createAction(type)
-    }), {})
+      ...acc, [transform(type)]: createAction(type)
+    }), {});
 }
