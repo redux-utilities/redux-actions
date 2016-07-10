1dc2449e1f10787f47efb3ab94f745c16fb63a56
Victor Alvarez <v.alvarez312@gmail.com>
2016-07-10 08:48:40 -0700

    simplify return logic; remove typeof check (#106)
    http://stackoverflow.com/a/4725697/2419669

--- src/handleActions.js
+++ src/handleActions.js
@@ -6,7 +6,5 @@ export default function handleActions(handlers, defaultState) {
   const reducers = ownKeys(handlers).map(type => handleAction(type, handlers[type]));
   const reducer = reduceReducers(...reducers);
 
-  return typeof defaultState !== 'undefined'
-    ? (state = defaultState, action) => reducer(state, action)
-    : reducer;
+  return (state = defaultState, action) => reducer(state, action);
 }
