347638c39a02a79b379b1cb1b424cdd317d1b9ed
VIctor Alvarez <v.alvarez312@gmail.com>
2016-07-30 16:08:14 -0700

    reduce combined actions; first pass

--- src/handleAction.js
+++ src/handleAction.js
@@ -1,22 +1,28 @@
 import isFunction from 'lodash/isFunction';
 import identity from 'lodash/identity';
 import isNil from 'lodash/isNil';
+import includes from 'lodash/includes';
 import isSymbol from 'lodash/isSymbol';
+import { FSA_TYPE_DELIMITER } from './combineActions';
 
-export default function handleAction(type, reducers, defaultState) {
-  const typeValue = isSymbol(type)
-    ? type
-    : type.toString();
+export default function handleAction(handlee, reducers, defaultState) {
+  const typeValue = isSymbol(handlee)
+    ? handlee
+    : handlee.toString().split(FSA_TYPE_DELIMITER);
 
   const [nextReducer, throwReducer] = isFunction(reducers)
     ? [reducers, reducers]
     : [reducers.next, reducers.throw].map(reducer => (isNil(reducer) ? identity : reducer));
 
   return (state = defaultState, action) => {
-    if (action.type !== typeValue) {
+    const { type } = action;
+    if (isSymbol(typeValue)) {
+      if (type !== typeValue) {
+        return state;
+      }
+    } else if (!includes(typeValue, type)) {
       return state;
     }
-
     return (action.error === true ? throwReducer : nextReducer)(state, action);
   };
 }
