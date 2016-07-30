d13c3ee407d317dd7e3610b8f19f55249b183607
VIctor Alvarez <v.alvarez312@gmail.com>
2016-07-30 16:10:37 -0700

    formatting

--- src/combineActions.js
+++ src/combineActions.js
@@ -5,7 +5,7 @@ import isEmpty from 'lodash/isEmpty';
 export const FSA_TYPE_DELIMITER = '|fsa-type-delimiter|';
 
 export default function combineActions(...actions) {
-  if (isEmpty(actions)|| !actions.every(isValidActionType)) {
+  if (isEmpty(actions) || !actions.every(isValidActionType)) {
     throw new TypeError('Expected each argument to be a string action type or an action creator');
   }
 
