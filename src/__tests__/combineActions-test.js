4cb286c844b0516ec54f6e2b932645d0ccfd6063
VIctor Alvarez <v.alvarez312@gmail.com>
2016-07-30 19:28:51 -0700

    add tests

---
+++ src/__tests__/combineActions-test.js
@@ -0,0 +1,33 @@
+import { combineActions, createAction } from '../';
+import { expect } from 'chai'
+
+describe('combineActions', () => {
+  it('should throw an error if any action is not a function or string', () => {
+    expect(
+      () => combineActions(1, 'ACTION_2')
+    ).to.throw(TypeError, 'Expected actions to be strings, symbols, or action creators');
+
+    expect(
+      () => combineActions('ACTION_1', () => {}, null)
+    ).to.throw(TypeError, 'Expected actions to be strings, symbols, or action creators');
+  });
+
+  it('should accept action creators and action type strings', () => {
+    const [ action1, action2 ] = [ createAction('ACTION_1'), createAction('ACTION_2') ];
+
+    expect(() => combineActions('ACTION_1', 'ACTION_2'))
+      .not.to.throw(Error);
+    expect(() => combineActions(action1, action2))
+      .not.to.throw(Error);
+    expect(() => combineActions(action1, action2, 'ACTION_3'))
+      .not.to.throw(Error);
+  });
+
+  it('should return a stringifiable object', () => {
+    const [ action1, action2 ] = [ createAction('ACTION_1'), createAction('ACTION_2') ];
+
+    expect(combineActions('ACTION_1', 'ACTION_2')).to.respondTo('toString');
+    expect(combineActions(action1, action2)).to.respondTo('toString');
+    expect(combineActions(action1, action2, 'ACTION_3')).to.respondTo('toString');
+  });
+});
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import createAction from './createAction';
 import handleAction from './handleAction';
 import handleActions from './handleActions';
+import combineActions from './combineActions'
 
 export {
   createAction,
   handleAction,
-  handleActions
+  handleActions,
+  combineActions,
 };
