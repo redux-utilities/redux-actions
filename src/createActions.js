12788e9f5ed5e1840ee5c2cf1fadfb836b9a7426
VIctor Alvarez <v.alvarez312@gmail.com>
2016-07-30 23:41:58 -0700

    WIP

--- src/createActions.js
+++ src/createActions.js
@@ -6,27 +6,45 @@ import isString from 'lodash/isString';
 import isFunction from 'lodash/isFunction';
 import createAction from './createAction';
 
+function isValidActionsMapValue(actionsMapValue) {
+  if (isFunction(actionsMapValue)) {
+    return true;
+  } else if (isPlainObject(actionsMapValue)) {
+    const { payload, meta } = actionsMapValue;
+    return isFunction(payload) && isFunction(meta);
+  }
+  return false;
+}
+
 function fromActionsMap(actionsMap) {
-  return reduce(actionsMap, (actionCreatorsMap, payloadCreator = identity, type) => {
-    if (!isFunction(payloadCreator)) {
-      throw new TypeError(`Expected function or undefined payload creator for ${type}`);
+  return reduce(actionsMap, (actionCreatorsMap, actionsMapValue = identity, type) => {
+    if (!isValidActionsMapValue(actionsMapValue)) {
+      throw new TypeError(`
+        Expected function, plain object with payload and meta keys, or undefined for ${type}`);
     }
-    return { ...actionCreatorsMap, [camelCase(type)]: createAction(type, payloadCreator) };
+    let actionCreator;
+    if (isPlainObject(actionsMapValue)) {
+      const { payload = identity, meta } = actionsMapValue
+      actionCreator = createAction(type, payload, meta);
+    } else {
+      actionCreator = createAction(type, actionsMapValue);
+    }
+    return { ...actionCreatorsMap, [camelCase(type)]: actionCreator };
   }, {});
 }
 
-function fromTypes(...types) {
+function fromActionTypeStrings(...types) {
   return fromActionsMap(
     types.reduce((actionsMap, action) => ({ ...actionsMap, [action]: undefined }), {})
   );
 }
 
-export default function createActions(actionsMap, ...types) {
-  if (types.every(isString)) {
+export default function createActions(actionsMap, ...actionTypes) {
+  if (actionTypes.every(isString)) {
     if (isString(actionsMap)) {
-      return fromTypes(actionsMap, ...types);
+      return fromActionTypeStrings(actionsMap, ...actionTypes);
     } else if (isPlainObject(actionsMap)) {
-      return { ...fromActionsMap(actionsMap), ...fromTypes(...types) };
+      return { ...fromActionsMap(actionsMap), ...fromActionTypeStrings(...actionTypes) };
     }
   }
   throw new TypeError('Expected (optional) object followed by string action types');
