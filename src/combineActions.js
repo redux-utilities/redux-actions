1fb1eee7576002d894a2948b9a32d6101d0b7aea
VIctor Alvarez <v.alvarez312@gmail.com>
2016-08-31 10:32:21 -0700

    reorder

--- src/combineActions.js
+++ src/combineActions.js
@@ -6,16 +6,6 @@ import isSymbol from 'lodash/isSymbol';
 
 export const ACTION_TYPE_DELIMITER = '|redux-action-delimiter|';
 
-export default function combineActions(...actionsTypes) {
-  if (!isValidActionTypes(actionsTypes)) {
-    throw new TypeError('Expected action types to be strings, symbols, or action creators');
-  }
-
-  const combinedActionType = actionsTypes.map(toString).join(ACTION_TYPE_DELIMITER);
-
-  return { toString: () => combinedActionType };
-}
-
 function isValidActionTypes(actionTypes) {
   if (isEmpty(actionTypes)) {
     return false;
@@ -25,4 +15,14 @@ function isValidActionTypes(actionTypes) {
 
 function isValidActionType(actionType) {
   return isString(actionType) || isFunction(actionType) || isSymbol(actionType);
+}
+
+export default function combineActions(...actionsTypes) {
+  if (!isValidActionTypes(actionsTypes)) {
+    throw new TypeError('Expected action types to be strings, symbols, or action creators');
+  }
+
+  const combinedActionType = actionsTypes.map(toString).join(ACTION_TYPE_DELIMITER);
+
+  return { toString: () => combinedActionType };
 }
