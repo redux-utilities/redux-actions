f0f8a1bc12e1229aaf3069da8c82a8a419aea24d
Victor Alvarez <v.alvarez312@gmail.com>
2016-12-04 02:00:02 -0800

    Revert "move ownKeys"
    This reverts commit 4263c44551ddc3eda8b75cd8a7986ff739981815.

--- src/handleActions.js
+++ src/handleActions.js
@@ -1,5 +1,5 @@
 import handleAction from './handleAction';
-import ownKeys from './utils/ownKeys';
+import ownKeys from './ownKeys';
 import reduceReducers from 'reduce-reducers';
 
 export default function handleActions(handlers, defaultState) {
