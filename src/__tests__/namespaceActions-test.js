f4fe4f56449b3c9f4cc8ba6e3c1e25019332b58f
Victor Alvarez <v.alvarez312@gmail.com>
2017-02-26 12:03:43 -0800

    WIP

--- src/__tests__/namespaceActions-test.js
+++ src/__tests__/namespaceActions-test.js
@@ -1,89 +1,84 @@
-/* global describe, it, beforeEach */
-import {nestActions, unnestActions} from './index'
-import {createActions} from 'redux-actions'
-import {expect} from 'chai'
+import { flattenActions, unflattenActions } from '../namespaceActions';
+import { expect } from 'chai';
 
-describe('createActions', () => {
-  describe('scoping', () => {
+describe('flattenActions', () => {
+  it('should flatten an action creators map with the default namespacer', () => {
     const actionsMap = {
-      'APP/COUNTER/INCREMENT': amount => ({amount}),
-      'APP/COUNTER/DECREMENT': amount => ({amount: -amount}),
-      'APP/NOTIFY': (username, message) => ({message: `${username}: ${message}`}),
-      LOGIN: username => ({username}),
-    }
-    let actionCreators
-
-    beforeEach(() => {
-      actionCreators = unnestActions(createActions(actionsMap, 'LOGOUT'))
-    })
-
-    it('should create top-level action creators', () => {
-      expect(actionCreators.login('yangmillstheory')).to.deep.equal({
-        type: 'LOGIN',
-        payload: {username: 'yangmillstheory'},
-      })
-      expect(actionCreators.logout()).to.deep.equal({type: 'LOGOUT'})
-    })
-
-    it('should create child action creators', () => {
-      expect(actionCreators.app.notify('yangmillstheory', 'Hello, World!'))
-        .to.deep.equal({
-          type: 'APP/NOTIFY',
-          message: 'yangmillstheory: Hello, World!',
-        })
-    })
-
-    it('should create deeply nested action creators', () => {
-      expect(actionCreators.app.counter.increment(1)).to.deep.equal({
-        type: 'APP/COUNTER/INCREMENT',
-        amount: 1,
-      })
-      expect(actionCreators.app.counter.decrement(1)).to.deep.equal({
-        type: 'APP/COUNTER/DECREMENT',
-        amount: -1,
-      })
-    })
-  })
-
-  describe('packing', () => {
-    it('should pack actions', () => {
-      const unnestedActions = {
-        APP: {
-          COUNTER: {
-            INCREMENT: amount => ({amount}),
-            DECREMENT: amount => ({amount: -amount}),
-          },
-          NOTIFY: (username, message) => ({message: `${username}: ${message}`}),
+      APP: {
+        COUNTER: {
+          INCREMENT: amount => ({ amount }),
+          DECREMENT: amount => ({ amount: -amount })
         },
-        LOGIN: username => ({username}),
-      }
+        NOTIFY: (username, message) => ({ message: `${username}: ${message}` })
+      },
+      LOGIN: username => ({ username })
+    };
 
-      expect(nestActions(unnestedActions)).to.deep.equal({
-        'APP/COUNTER/INCREMENT': unnestedActions.APP.COUNTER.INCREMENT,
-        'APP/COUNTER/DECREMENT': unnestedActions.APP.COUNTER.DECREMENT,
-        'APP/NOTIFY': unnestedActions.APP.NOTIFY,
-        'LOGIN': unnestedActions.LOGIN,
-      })
-    })
+    expect(flattenActions(actionsMap)).to.deep.equal({
+      'APP/COUNTER/INCREMENT': actionsMap.APP.COUNTER.INCREMENT,
+      'APP/COUNTER/DECREMENT': actionsMap.APP.COUNTER.DECREMENT,
+      'APP/NOTIFY': actionsMap.APP.NOTIFY,
+      LOGIN: actionsMap.LOGIN
+    });
+  });
 
-    it('should be case insensitive', () => {
-      const unnestedActions = {
-        app: {
-          counter: {
-            increment: amount => ({amount}),
-            decrement: amount => ({amount: -amount}),
-          },
-          notify: (username, message) => ({message: `${username}: ${message}`}),
+  it('should be casesensitive', () => {
+    const actionsMap = {
+      app: {
+        counter: {
+          increment: amount => ({ amount }),
+          decrement: amount => ({ amount: -amount })
         },
-        login: username => ({username}),
-      }
+        notify: (username, message) => ({ message: `${username}: ${message}` })
+      },
+      login: username => ({ username })
+    };
 
-      expect(nestActions(unnestedActions)).to.deep.equal({
-        'app/counter/increment': unnestedActions.app.counter.increment,
-        'app/counter/decrement': unnestedActions.app.counter.decrement,
-        'app/notify': unnestedActions.app.notify,
-        'login': unnestedActions.login,
-      })
-    })
-  })
-})
+    expect(flattenActions(actionsMap)).to.deep.equal({
+      'app/counter/increment': actionsMap.app.counter.increment,
+      'app/counter/decrement': actionsMap.app.counter.decrement,
+      'app/notify': actionsMap.app.notify,
+      login: actionsMap.login
+    });
+  });
+
+  it('should use a custom namespace string', () => {
+    const actionsMap = {
+      APP: {
+        COUNTER: {
+          INCREMENT: amount => ({ amount }),
+          DECREMENT: amount => ({ amount: -amount })
+        },
+        NOTIFY: (username, message) => ({ message: `${username}: ${message}` })
+      },
+      LOGIN: username => ({ username })
+    };
+
+    expect(flattenActions(actionsMap, '-')).to.deep.equal({
+      'APP-COUNTER-INCREMENT': actionsMap.APP.COUNTER.INCREMENT,
+      'APP-COUNTER-DECREMENT': actionsMap.APP.COUNTER.DECREMENT,
+      'APP-NOTIFY': actionsMap.APP.NOTIFY,
+      LOGIN: actionsMap.LOGIN
+    });
+  });
+});
+
+describe('unflattenActions', () => {
+  beforeEach(() => {
+    this.actionsMap = {
+      'APP/COUNTER/INCREMENT': amount => ({ amount }),
+      'APP/COUNTER/DECREMENT': amount => ({ amount: -amount }),
+      'APP/NOTIFY': (username, message) => ({ message: `${username}: ${message}` }),
+      LOGIN: username => ({ username })
+    };
+  });
+
+  it('should unflatten a flattened actions map', () => {
+    const actionsMap = unflattenActions(this.actionsMap);
+
+    expect(actionsMap.LOGIN('yangmillstheory')).to.deep.equal({ username: 'yangmillstheory' });
+    expect(actionsMap.APP.COUNTER.INCREMENT(100)).to.deep.equal({ amount: 100 });
+    expect(actionsMap.APP.COUNTER.DECREMENT(100)).to.deep.equal({ amount: -100 });
+    expect(actionsMap.APP.NOTIFY('yangmillstheory', 'Hello World')).to.deep.equal({ message: 'yangmillstheory: Hello World' });
+  });
+});
