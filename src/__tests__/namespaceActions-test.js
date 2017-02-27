9fee0c489d9360751df6709df3a3f213ef48835d
Victor Alvarez <v.alvarez312@gmail.com>
2017-02-26 19:53:10 -0800

    WIP

--- src/__tests__/namespaceActions-test.js
+++ src/__tests__/namespaceActions-test.js
@@ -1,108 +1,110 @@
 import { flattenActions, unflattenActions } from '../namespaceActions';
 import { expect } from 'chai';
 
-describe('flattenActions', () => {
-  it('should flatten an action creators map with the default namespacer', () => {
-    const actionsMap = {
-      APP: {
-        COUNTER: {
-          INCREMENT: amount => ({ amount }),
-          DECREMENT: amount => ({ amount: -amount })
+describe('namespacing actions', () => {
+  describe('flattenActions', () => {
+    it('should flatten an action creators map with the default namespacer', () => {
+      const actionsMap = {
+        APP: {
+          COUNTER: {
+            INCREMENT: amount => ({ amount }),
+            DECREMENT: amount => ({ amount: -amount })
+          },
+          NOTIFY: (username, message) => ({ message: `${username}: ${message}` })
         },
-        NOTIFY: (username, message) => ({ message: `${username}: ${message}` })
-      },
-      LOGIN: username => ({ username })
-    };
+        LOGIN: username => ({ username })
+      };
 
-    expect(flattenActions(actionsMap)).to.deep.equal({
-      'APP/COUNTER/INCREMENT': actionsMap.APP.COUNTER.INCREMENT,
-      'APP/COUNTER/DECREMENT': actionsMap.APP.COUNTER.DECREMENT,
-      'APP/NOTIFY': actionsMap.APP.NOTIFY,
-      LOGIN: actionsMap.LOGIN
+      expect(flattenActions(actionsMap)).to.deep.equal({
+        'APP/COUNTER/INCREMENT': actionsMap.APP.COUNTER.INCREMENT,
+        'APP/COUNTER/DECREMENT': actionsMap.APP.COUNTER.DECREMENT,
+        'APP/NOTIFY': actionsMap.APP.NOTIFY,
+        LOGIN: actionsMap.LOGIN
+      });
     });
-  });
 
-  it('should do nothing to an already flattened map', () => {
-    const actionsMap = {
-      INCREMENT: amount => ({ amount }),
-      DECREMENT: amount => ({ amount: -amount }),
-      LOGIN: username => ({ username })
-    };
+    it('should do nothing to an already flattened map', () => {
+      const actionsMap = {
+        INCREMENT: amount => ({ amount }),
+        DECREMENT: amount => ({ amount: -amount }),
+        LOGIN: username => ({ username })
+      };
 
-    expect(flattenActions(actionsMap)).to.deep.equal(actionsMap);
-  });
+      expect(flattenActions(actionsMap)).to.deep.equal(actionsMap);
+    });
 
-  it('should be case-sensitive', () => {
-    const actionsMap = {
-      app: {
-        counter: {
-          increment: amount => ({ amount }),
-          decrement: amount => ({ amount: -amount })
+    it('should be case-sensitive', () => {
+      const actionsMap = {
+        app: {
+          counter: {
+            increment: amount => ({ amount }),
+            decrement: amount => ({ amount: -amount })
+          },
+          notify: (username, message) => ({ message: `${username}: ${message}` })
         },
-        notify: (username, message) => ({ message: `${username}: ${message}` })
-      },
-      login: username => ({ username })
-    };
+        login: username => ({ username })
+      };
 
-    expect(flattenActions(actionsMap)).to.deep.equal({
-      'app/counter/increment': actionsMap.app.counter.increment,
-      'app/counter/decrement': actionsMap.app.counter.decrement,
-      'app/notify': actionsMap.app.notify,
-      login: actionsMap.login
+      expect(flattenActions(actionsMap)).to.deep.equal({
+        'app/counter/increment': actionsMap.app.counter.increment,
+        'app/counter/decrement': actionsMap.app.counter.decrement,
+        'app/notify': actionsMap.app.notify,
+        login: actionsMap.login
+      });
     });
-  });
 
-  it('should use a custom namespace string', () => {
-    const actionsMap = {
-      APP: {
-        COUNTER: {
-          INCREMENT: amount => ({ amount }),
-          DECREMENT: amount => ({ amount: -amount })
+    it('should use a custom namespace string', () => {
+      const actionsMap = {
+        APP: {
+          COUNTER: {
+            INCREMENT: amount => ({ amount }),
+            DECREMENT: amount => ({ amount: -amount })
+          },
+          NOTIFY: (username, message) => ({ message: `${username}: ${message}` })
         },
-        NOTIFY: (username, message) => ({ message: `${username}: ${message}` })
-      },
-      LOGIN: username => ({ username })
-    };
+        LOGIN: username => ({ username })
+      };
 
-    expect(flattenActions(actionsMap, '-')).to.deep.equal({
-      'APP-COUNTER-INCREMENT': actionsMap.APP.COUNTER.INCREMENT,
-      'APP-COUNTER-DECREMENT': actionsMap.APP.COUNTER.DECREMENT,
-      'APP-NOTIFY': actionsMap.APP.NOTIFY,
-      LOGIN: actionsMap.LOGIN
+      expect(flattenActions(actionsMap, '-')).to.deep.equal({
+        'APP-COUNTER-INCREMENT': actionsMap.APP.COUNTER.INCREMENT,
+        'APP-COUNTER-DECREMENT': actionsMap.APP.COUNTER.DECREMENT,
+        'APP-NOTIFY': actionsMap.APP.NOTIFY,
+        LOGIN: actionsMap.LOGIN
+      });
     });
   });
-});
 
-describe('unflattenActions', () => {
-  it('should unflatten a flattened actions map', () => {
-    const actionsMap = unflattenActions({
-      'APP/COUNTER/INCREMENT': amount => ({ amount }),
-      'APP/COUNTER/DECREMENT': amount => ({ amount: -amount }),
-      'APP/NOTIFY': (username, message) => ({ message: `${username}: ${message}` }),
-      LOGIN: username => ({ username })
+  describe('unflattenActions', () => {
+    it('should unflatten a flattened actions map and camel-case keys', () => {
+      const actionsMap = unflattenActions({
+        'APP/COUNTER/INCREMENT': amount => ({ amount }),
+        'APP/COUNTER/DECREMENT': amount => ({ amount: -amount }),
+        'APP/NOTIFY': (username, message) => ({ message: `${username}: ${message}` }),
+        LOGIN: username => ({ username })
+      });
+
+      expect(actionsMap.login('yangmillstheory')).to.deep.equal({ username: 'yangmillstheory' });
+      expect(actionsMap.app.notify('yangmillstheory', 'Hello World')).to.deep.equal({
+        message: 'yangmillstheory: Hello World'
+      });
+      expect(actionsMap.app.counter.increment(100)).to.deep.equal({ amount: 100 });
+      expect(actionsMap.app.counter.decrement(100)).to.deep.equal({ amount: -100 });
     });
 
-    expect(actionsMap.login('yangmillstheory')).to.deep.equal({ username: 'yangmillstheory' });
-    expect(actionsMap.app.notify('yangmillstheory', 'Hello World')).to.deep.equal({
-      message: 'yangmillstheory: Hello World'
-    });
-    expect(actionsMap.app.counter.increment(100)).to.deep.equal({ amount: 100 });
-    expect(actionsMap.app.counter.decrement(100)).to.deep.equal({ amount: -100 });
-  });
+    it('should unflatten a flattened actions map with custom namespace', () => {
+      const actionsMap = unflattenActions({
+        'APP--COUNTER--INCREMENT': amount => ({ amount }),
+        'APP--COUNTER--DECREMENT': amount => ({ amount: -amount }),
+        'APP--NOTIFY': (username, message) => ({ message: `${username}: ${message}` }),
+        LOGIN: username => ({ username })
+      }, '--');
 
-  it('should unflatten a flattened actions map with custom namespace', () => {
-    const actionsMap = unflattenActions({
-      'APP--COUNTER--INCREMENT': amount => ({ amount }),
-      'APP--COUNTER--DECREMENT': amount => ({ amount: -amount }),
-      'APP--NOTIFY': (username, message) => ({ message: `${username}: ${message}` }),
-      LOGIN: username => ({ username })
-    }, '--');
-
-    expect(actionsMap.login('yangmillstheory')).to.deep.equal({ username: 'yangmillstheory' });
-    expect(actionsMap.app.notify('yangmillstheory', 'Hello World')).to.deep.equal({
-      message: 'yangmillstheory: Hello World'
+      expect(actionsMap.login('yangmillstheory')).to.deep.equal({ username: 'yangmillstheory' });
+      expect(actionsMap.app.notify('yangmillstheory', 'Hello World')).to.deep.equal({
+        message: 'yangmillstheory: Hello World'
+      });
+      expect(actionsMap.app.counter.increment(100)).to.deep.equal({ amount: 100 });
+      expect(actionsMap.app.counter.decrement(100)).to.deep.equal({ amount: -100 });
     });
-    expect(actionsMap.app.counter.increment(100)).to.deep.equal({ amount: 100 });
-    expect(actionsMap.app.counter.decrement(100)).to.deep.equal({ amount: -100 });
   });
 });
