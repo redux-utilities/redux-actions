c87508b81307e52c9a82be7a1961323247531555
Lucretiel <Lucretiel@users.noreply.github.com>
2017-05-10 00:59:11 -0400

    more pre-processing in createAction (#210)

--- src/createAction.js
+++ src/createAction.js
@@ -1,6 +1,5 @@
 import identity from 'lodash/identity';
 import isFunction from 'lodash/isFunction';
-import isUndefined from 'lodash/isUndefined';
 import isNull from 'lodash/isNull';
 import invariant from 'invariant';
 
@@ -10,35 +9,34 @@ export default function createAction(type, payloadCreator = identity, metaCreato
     'Expected payloadCreator to be a function, undefined or null'
   );
 
-  const finalPayloadCreator = isNull(payloadCreator)
+  const finalPayloadCreator = isNull(payloadCreator) || payloadCreator === identity
     ? identity
-    : payloadCreator;
+    : (head, ...args) => (head instanceof Error
+      ? head : payloadCreator(head, ...args));
+
+  const hasMeta = isFunction(metaCreator);
+  const typeString = type.toString();
 
   const actionCreator = (...args) => {
-    const hasError = args[0] instanceof Error;
+    const payload = finalPayloadCreator(...args);
+    const action = { type };
 
-    const action = {
-      type
-    };
-
-    const payload = hasError ? args[0] : finalPayloadCreator(...args);
-    if (!isUndefined(payload)) {
-      action.payload = payload;
-    }
-
-    if (hasError || payload instanceof Error) {
-      // Handle FSA errors where the payload is an Error object. Set error.
+    if (payload instanceof Error) {
       action.error = true;
     }
 
-    if (isFunction(metaCreator)) {
+    if (payload !== undefined) {
+      action.payload = payload;
+    }
+
+    if (hasMeta) {
       action.meta = metaCreator(...args);
     }
 
     return action;
   };
 
-  actionCreator.toString = () => type.toString();
+  actionCreator.toString = () => typeString;
 
   return actionCreator;
 }
