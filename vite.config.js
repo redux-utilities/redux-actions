4265566bc465e3727ce29be9ae6eb00ba2d4bed3
Alexander Heimbuch <github@heimbu.ch>
2023-01-21 19:53:11 +0100

    chore: only support ESM targets

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   },
   build: {
     lib: {
+      formats: ['es'],
       // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, 'src/index.js'),
       name: 'ReduxActions',
